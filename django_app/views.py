547e686103a7a8f2752b08b987c96e7c8f55c707
ZuhriddinRW <shodmonovzuhriddin2009@gmail.com>
2025-11-11 08:10:23 +0500

    updated

--- django_app/views.py
+++ django_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.shortcuts import render, redirect
 
 
 def index(request) :
@@ -23,3 +24,13 @@ def design(request) :
 
 def news(request) :
     return render ( request, 'news.html' )
+
+# def SignIn(request):
+#     if request.method == "POST":
+#         email = request.POST.get('email')
+#         password = request.POST.get('password')
+#
+#         user = authenticate(request, email=email, password=password)
+#         if user:
+#             login(request, user)
+#             return redirect('index')
